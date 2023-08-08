26482e87a3a6d4ae9da332ac30356e4feb9ca630
NoHistory001 <140395154+NoHistory001@users.noreply.github.com>
2023-08-08 15:04:13 +0100

    Add files via upload

---
+++ My Portfolio/My Portfolio.sql
@@ -0,0 +1,12 @@
+your_portfolio/
+    |-- templates/
+    |     |-- index.html
+    |     |-- about.html
+    |-- static/
+    |     |-- css/
+    |          |-- styles.css
+    |     |-- images/
+    |          |-- (place any images you want to use here)
+    |     |-- js/
+    |          |-- (place any JavaScript files you want to use here)
+    |-- app.py
